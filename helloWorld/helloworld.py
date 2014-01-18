9d38e8bb7e4702618baf9f313d3b3ad5f2cf8359
xiaomijimu <jimzhuang.cn@gmail.com>
2014-01-18 22:22:26 +0800

    deleted 2 files

--- helloWorld/helloworld.py
+++ helloWorld/helloworld.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python
 # Filename : helloworld.py
 print 'Hello World'
+
+i = 5
+print i
+i = i + 1
+print i
+
+s = '''This is a multi-line string.
+This is the second line.'''
+print s
