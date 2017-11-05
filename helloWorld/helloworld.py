00879e5971d76a1e4e6787767936b25a19311650
xiaomijimu <jimzhuang.cn+programming@gmail.com>
2017-11-05 21:09:17 +0800

    correct the print function

--- helloWorld/helloworld.py
+++ helloWorld/helloworld.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # Filename : helloworld.py
-print 'Hello World'
+print ('Hello World')
 
 i = 5
-print i
+print (i)
 i = i + 1
-print i
+print (i)
 
 s = '''This is a multi-line string.
 This is the second line.'''
-print s
+print (s)
